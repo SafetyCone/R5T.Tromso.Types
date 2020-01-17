490aeaa617ff4c3cd437b39495eef8d861bb0a52
MinexAutomation <minex.automation@gmail.com>
2020-01-16 23:48:24 -0800

    Modified for the new IServiceBuilder<IServiceProvider> interface.

--- source/R5T.Tromso.Types/Code/Extensions/IBuildableServiceExtensions.cs
+++ source/R5T.Tromso.Types/Code/Extensions/IBuildableServiceExtensions.cs
@@ -5,7 +5,7 @@ namespace R5T.Tromso.Types
 {
     public static class IBuildableServiceExtensions
     {
-        public static IServiceBuilder GetServiceBuilder(this IBuildableService buildableService)
+        public static IServiceBuilder<IServiceProvider> GetServiceBuilder(this IBuildableService buildableService)
         {
             var serviceBuilder = new ServiceBuilder(buildableService);
             return serviceBuilder;
