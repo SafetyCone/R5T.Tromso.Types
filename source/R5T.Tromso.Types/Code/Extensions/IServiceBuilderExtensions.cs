4b73a0cb00a9de2f3d7ad8a8db875df2ead38a1c
MinexAutomation <minex.automation@gmail.com>
2020-01-19 17:49:44 -0800

    Added IServiceBuilder extensions.

---
+++ source/R5T.Tromso.Types/Code/Extensions/IServiceBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using R5T.Dacia;
+
+
+namespace R5T.Tromso
+{
+    public static class IServiceBuilderExtensions
+    {
+        public static void Build(this IServiceBuilder serviceBuilder)
+        {
+            serviceBuilder.Build(ServiceProviderHelper.EmptyServiceProvider.Value);
+        }
+
+        public static IServiceBuilder<TService> BuildFluent<TService>(this IServiceBuilder<TService> serviceBuilder)
+        {
+            serviceBuilder.Build();
+
+            return serviceBuilder;
+        }
+    }
+}
